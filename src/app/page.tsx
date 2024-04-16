08dfa7baa0ca5383496cffdaa80c6887b34dc83c
YeMarn <yemarn.510@gmail.com>
2024-04-16 15:03:50 +0700

    Added a function to go to the next step

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,38 +4,84 @@ import StepOne, { StepOneParams } from "@/components/step-1";
 import { Item } from "@/models/item.models";
 import { useState, useEffect } from "react";
 import { Steps } from 'antd';
+import StepTwo, { StepTwoParams } from "@/components/step-2";
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@ant-design/icons';
 
+const STEPS = ['Add Items', 'Add People', 'Assign People & Items', 'Review & Split'];
 
 export default function Home() {
 
   const [currentStep, setCurrentStep] = useState(0);
-
   const [items, setItems] = useState<Item[]>([]);
-
-  const steps = [
-    {
-      title: 'Add Items',
-    },  
-    {
-      title: 'Add People',
-    },
-    {
-      title: 'Assign People & Items',
-    },
-    {
-      title: 'Review & Split',
-    },
-  ];
+  const steps = STEPS.map(each => ({ title: each }));
 
   useEffect(() => {
     setCurrentStep(0);
     setItems([ new Item({}) ]);
-  }, [currentStep]);
+  }, []);
 
-  const StepOneParams: StepOneParams = {
+  function goNext(): void {
+    switch (currentStep) {
+      case 0:
+        if (isStepOneValid()) {
+          setCurrentStep(currentStep + 1);
+        }
+        break;
+      case 1:
+        
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  }
+
+  function goBack(): void {
+    if (currentStep > 0) {
+      setCurrentStep(currentStep - 1);
+    }
+  }
+
+  function isStepOneValid(): boolean {
+    const checkingItems = items.map(each => {
+      each.isValid();
+      return each;
+    });
+    const isValid = checkingItems.every(each => each.isValid());
+    if (isValid) {
+      return true;
+    }
+    setItems([...checkingItems]);
+    return false;
+  }
+
+  function getCurrentUI(): JSX.Element {
+    switch (currentStep) {
+      case 0:
+        return <StepOne params={stepOneParams} />;
+      case 1:
+        return <StepTwo params={stepTwoParams} />;
+      // case 2:
+      //   return <StepThree params={stepThreeParams} />;
+      // case 3:
+      //   return <StepFour params={stepFourParams} />;
+      default:
+        return <div>Not Implemented yet!</div>;
+    }
+  }
+
+
+
+
+  const stepOneParams: StepOneParams = {
     items,
     setItems,
   };
+
+  const stepTwoParams: StepTwoParams = {
+
+  }
 
   return (
     <main className="max-w-[1100px] min-w-[50vw]  w-fit h-screen m-auto py-10">
@@ -51,7 +97,25 @@ export default function Home() {
 
 
         <div className="py-20">
-          <StepOne params={ StepOneParams } />
+          { getCurrentUI() }
+        </div>
+        
+        <div className="flex flex-row justify-end gap-5">
+          <div className={`flex flex-row gap-3 items-center cursor-pointer hover:opacity-50 ${ currentStep === 0 && 'cursor-not-allowed opacity-50'}`}
+               onClick={ () => goBack() }>
+            <p className="mb-0 w-auto text-main">Go Back</p>
+            <div className="w-10 h-10 flex justify-center items-center rounded-full border border-main">
+              <ArrowLeftOutlined className="text-main" />
+            </div>
+          </div>
+
+          <div className={`flex flex-row gap-3 items-center cursor-pointer hover:opacity-50 ${ currentStep === (steps.length - 1) && 'cursor-not-allowed opacity-50'}`}
+               onClick={ () => goNext() }>
+            <div className="w-10 h-10 flex justify-center items-center rounded-full border border-main">
+              <ArrowRightOutlined className="text-main" />
+            </div>
+            <p className="mb-0 w-auto text-main">Go Next</p>
+          </div>
         </div>
       </section>
     </main>
---
+++ src/components/step-2.tsx
@@ -0,0 +1,13 @@
+
+
+export interface StepTwoParams {
+
+}
+
+export default function StepTwo(params: {params: StepTwoParams }): JSX.Element {
+
+
+  return <>
+    <div>Not Implemented yet!</div>
+  </>
+}
