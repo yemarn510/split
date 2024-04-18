d9d91ac9fb3fb1c7d00d16cd502d79dfd741cf10
YeMarn <yemarn.510@gmail.com>
2024-04-18 18:16:01 +0700

    Fixed zooming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <ConfigProvider
         theme={{
