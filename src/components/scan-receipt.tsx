84f57304b69eb7f559a07d1271898523bfcf0454
YeMarn <yemarn.510@gmail.com>
2024-04-21 14:44:49 +0700

    added error message

--- src/components/scan-receipt.tsx
+++ src/components/scan-receipt.tsx
@@ -1,7 +1,7 @@
 import { Person } from "@/models/person.models";
 import { ScanResponse, Scanner } from "@/models/scanner.models";
-import { InboxOutlined, UserOutlined } from "@ant-design/icons";
-import { Modal, Popover, Button, UploadProps, message, Upload } from "antd";
+import { InboxOutlined, UserOutlined, LoadingOutlined } from "@ant-design/icons";
+import { Modal, Popover, Button, UploadProps, message, Upload, Tooltip } from "antd";
 
 import { useEffect, useState } from "react";
 import RoundedAvatar from "./custom-avatar";
@@ -28,10 +28,21 @@ export default function ScanReceipt(params: ScanReceiptParams): JSX.Element {
   const [originalItem, setOriginalItem] = useState<Item | null>(null);
   const [paidByIndex, setPaidByIndex] = useState<number | null>(null);
   const [currentIndex, setCurrentIndex] = useState<number | null>(null);
+  const [showContent, setShowContent] = useState<boolean>(true);
+
+  const [error, setError] = useState<{[key: string]: string}>({});
 
   useEffect(() => {
     setScannedItems([]);
+    setError({});
   }, [params.openScanPopup]);
+
+  useEffect( () => {
+    setShowContent(false);
+    setTimeout( () => {
+      setShowContent(true);
+    }, 1)
+  }, [error])
 
 
   const props: UploadProps = {
@@ -43,6 +54,8 @@ export default function ScanReceipt(params: ScanReceiptParams): JSX.Element {
     listType: "picture",
     action: 'https://xw3pr7ak-7dqrsyftta-de.a.run.app/extract_data?output_language=English',
     onChange(info) {
+      deleteError('image');
+
       switch (info.file.status) {
         case 'uploading':
           if (!loading) {
@@ -64,6 +77,9 @@ export default function ScanReceipt(params: ScanReceiptParams): JSX.Element {
   };
 
   function scanImage(): void {
+    if (!scanIsValid()) {
+      return;
+    };
     if (!params.scanner.response) {
       messageApi.error('Please upload a file first');
       return;
@@ -74,6 +90,23 @@ export default function ScanReceipt(params: ScanReceiptParams): JSX.Element {
       paidBy: params.scanner.paidBy,
     }));
     setScannedItems(scannedItems);
+  }
+
+  function scanIsValid(): boolean {
+    const newError: { [key: string]: string } = {};
+    if (!params.scanner.paidBy) {
+      newError.paidBy = 'Please choose who paid';
+    }
+    if (!params.scanner.response) {
+      newError.image = 'Please upload an image';
+    }
+    setError(newError);
+    return Object.keys(newError).length === 0;
+  }
+
+  function deleteError(key: string): void {
+    delete error[key];
+    setError(error);
   }
 
   const itemParams: ItemTableParams = {
@@ -99,50 +132,61 @@ export default function ScanReceipt(params: ScanReceiptParams): JSX.Element {
     }
 
     return <>
-      <div className="h-fit max-h-[350px] my-3 overflow-auto">
-        <div className="flex flex-col">
-          <Popover content={() => 
-                    (PersonList({ 
-                      profiles: params.people,
-                      scanner: params.scanner,
-                      setScanner: params.setScanner,
-                      setOpen: setOpen,
-                    }))
-                  }
-                  open={open}
-                  title="Choose Who Paid">
-            {
-              params.scanner.paidBy?.profile
-              ? <div className="flex flex-row gap-5 justify-center items-center"
-                      onClick={ () => setOpen(!open)}>
-                <h5 className="w-auto">Paid By</h5>
-                <div className="w-auto">
-                  <RoundedAvatar person={params.scanner.paidBy}/>
-                </div> 
-              </div>
-              : <Button className="w-full h-8 min-h-8"
-                        icon={<UserOutlined />}
-                        onClick={ () => setOpen(!open)}>
-                Choose Paid By
-              </Button>
-            }
-          </Popover>
-          <hr className="w-full my-3" />
-          <div className="w-auto h-auto">
-            <Upload {...props}>
-              <Button icon={<InboxOutlined />}
-                      className="w-full">
-                Click to upload
-              </Button>
-            </Upload>
+      <div className="h-fit max-h-[350px] my-3 overflow-auto ">
+        {
+          showContent &&
+          <div className="flex flex-col">
+            <Popover content={() => 
+                      (PersonList({ 
+                        profiles: params.people,
+                        scanner: params.scanner,
+                        setScanner: params.setScanner,
+                        setOpen: setOpen,
+                        deleteError: deleteError,
+                      }))
+                    }
+                    open={open}
+                    title="Choose Who Paid">
+              {
+                params.scanner.paidBy?.profile
+                ? <div className="flex flex-row gap-5 justify-center items-center"
+                        onClick={ () => {
+                          setOpen(!open);
+                        }}>
+                  <h5 className="w-auto">Paid By</h5>
+                  <div className="w-auto">
+                    <RoundedAvatar person={params.scanner.paidBy}/>
+                  </div> 
+                </div>
+                : <Button className="w-full h-8 min-h-8"
+                          icon={<UserOutlined />}
+                          onClick={ () => setOpen(!open)}>
+                  Choose Paid By
+                </Button>
+              }
+              <small className="text-danger w-full mx-auto">{ error.paidBy  || ''}</small>
+            </Popover>
+
+            <hr className="w-full my-3" />
+            
+            <div className="w-auto h-auto">
+              <Upload { ...props }>
+                <Button icon={<InboxOutlined />}
+                        className="w-full">
+                  Click to upload
+                </Button>
+              </Upload>
+              <small className="text-danger w-full mx-auto">{ error.image || ''}</small>
+            </div>
           </div>
-        </div>
+        }
       </div>
       
       <Button type="primary"
-        className="w-full max-h-8"
-        disabled={!params.scanner.paidBy || !params.scanner.response}
-        onClick={() => scanImage() }>
+              className="w-full max-h-8"
+              disabled={loading}
+              icon={ loading ? <LoadingOutlined /> : <></>}
+              onClick={() => scanImage() }>
         See the result
       </Button>
     </>
@@ -161,7 +205,13 @@ export default function ScanReceipt(params: ScanReceiptParams): JSX.Element {
   </>
 }
 
-export function PersonList(params: { profiles: Person[], scanner: Scanner, setScanner: Function, setOpen: Function}): JSX.Element {
+export function PersonList(params: { 
+  profiles: Person[], 
+  scanner: Scanner, 
+  setScanner: Function, 
+  setOpen: Function
+  deleteError: Function
+}): JSX.Element {
   return <div className="w-[200px] h-16 flex flex-row gap-5 justify-between overflow-x-auto">
     {
       params.profiles.map((person, index) => {
@@ -172,6 +222,7 @@ export function PersonList(params: { profiles: Person[], scanner: Scanner, setSc
                           paidBy: person,
                         });
                         params.setOpen(false);
+                        params.deleteError('paidBy');
                       }
                     }>
           <RoundedAvatar person={person} />
