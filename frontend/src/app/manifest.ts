719b76f111391b7b4fb29b0a4fdfcd489d520409
YeMarn <yemarn.510@gmail.com>
2025-04-17 09:59:13 +0700

    change color

--- frontend/src/app/manifest.ts
+++ frontend/src/app/manifest.ts
@@ -7,8 +7,8 @@ export default function manifest(): MetadataRoute.Manifest {
     description: 'A Progressive Web App for Splitting bills among friends',
     start_url: '/',
     display: 'standalone',
-    background_color: '#EADBC8',
-    theme_color: '#102C57',
+    background_color: '#102C57', 
+    theme_color: '#EADBC8',
     icons: [
       {
         src: '/images/favicon.png',
